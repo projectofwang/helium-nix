b85d98b7417874f184dd8cc58f1e23f8c7e758ed
projectofwang <projectofwang@gmail.com>
2026-09-14 11:46:52 +0700

    feat: add Home Manager module

---
+++ modules/home-manager.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.helium;
+  package = cfg.package.override { flags = cfg.flags; };
+in {
+  options.programs.helium = {
+    enable = lib.mkEnableOption "Helium Browser";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.helium;
+      defaultText = lib.literalExpression "pkgs.helium";
+      description = "Helium package to install.";
+    };
+
+    flags = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "Additional Chromium/Helium command-line flags.";
+    };
+
+    policies = lib.mkOption {
+      type = lib.types.attrs;
+      default = { };
+      description = "Managed Helium/Chromium policies written as JSON.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [ package ];
+
+    xdg.configFile."helium/policies/managed/nixos.json" = lib.mkIf (cfg.policies != { }) {
+      text = builtins.toJSON cfg.policies;
+    };
+  };
+}
