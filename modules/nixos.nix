7daf37ea835cf93a69600729c0fadc735715f4c8
projectofwang <projectofwang@gmail.com>
2026-09-14 11:46:49 +0700

    feat: add NixOS module

---
+++ modules/nixos.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.helium;
+  package = cfg.package.override { flags = cfg.flags; };
+in {
+  options.programs.helium = {
+    enable = lib.mkEnableOption "Helium Browser";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.helium;
+      defaultText = lib.literalExpression "pkgs.helium";
+      description = "Helium package to install.";
+    };
+
+    flags = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "Additional Chromium/Helium command-line flags.";
+    };
+
+    policies = lib.mkOption {
+      type = lib.types.attrs;
+      default = { };
+      description = "Managed Helium/Chromium policies written as JSON.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ package ];
+
+    environment.etc."chromium/policies/managed/helium-nixos.json" = lib.mkIf (cfg.policies != { }) {
+      text = builtins.toJSON cfg.policies;
+    };
+
+    environment.etc."helium/policies/managed/helium-nixos.json" = lib.mkIf (cfg.policies != { }) {
+      text = builtins.toJSON cfg.policies;
+    };
+  };
+}
