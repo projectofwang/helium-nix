e5782f75a40e2e6864d260a2dcd16fc9a41dfeb1
projectofwang <projectofwang@gmail.com>
2026-09-19 12:27:57 +0700

    fix: keep Home Manager smoke test self-contained

--- flake.nix
+++ flake.nix
@@ -101,8 +101,6 @@
                       home.username = "ci";
                       home.homeDirectory = "/tmp/helium-home-manager-test";
                       home.stateVersion = "26.05";
-                      nix.enable = true;
-                      nix.package = pkgs.nix;
                       programs.helium = {
                         enable = true;
                         package = customPackage;
